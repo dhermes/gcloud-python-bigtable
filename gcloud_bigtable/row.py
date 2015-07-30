9cd2a1b27bc463fb33f4e36683de313bdc324f4c
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-30 14:01:36 -0700

    Adding row key to Row class.
    Also adding list of protobuf mutations to track for
    a request.

--- gcloud_bigtable/row.py
+++ gcloud_bigtable/row.py
@@ -15,15 +15,29 @@
 """User friendly container for Google Cloud Bigtable Row."""
 
 
+import six
+
+
 class Row(object):
     """Representation of a Google Cloud Bigtable Column Row.
 
+    :type row_key: bytes (or string)
+    :param row_key: The key for the current row.
+
     :type table: :class:`.table.Table`
     :param table: The table that owns the row.
+
+    :raises: :class:`TypeError` if the ``row_key`` is not bytes or string.
     """
 
-    def __init__(self, table):
+    def __init__(self, row_key, table):
+        if isinstance(row_key, six.text_type):
+            row_key = row_key.encode('utf-8')
+        if not isinstance(row_key, bytes):
+            raise TypeError('Row key must be bytes.')
+        self._row_key = row_key
         self._table = table
+        self._pb_mutations = []
 
     @property
     def table(self):
@@ -33,3 +47,12 @@ class Row(object):
         :returns: The table stored on the row.
         """
         return self._table
+
+    @property
+    def row_key(self):
+        """Getter for row's key.
+
+        :rtype: bytes
+        :returns: The key for the row.
+        """
+        return self._row_key
--- gcloud_bigtable/test_row.py
+++ gcloud_bigtable/test_row.py
@@ -25,12 +25,36 @@ class TestRow(unittest2.TestCase):
     def _makeOne(self, *args, **kwargs):
         return self._getTargetClass()(*args, **kwargs)
 
-    def test_constructor(self):
+    def _constructor_helper(self, row_key, row_key_expected=None):
         table = object()
-        row = self._makeOne(table)
+        row = self._makeOne(row_key, table)
+        row_key_val = row_key_expected or row_key
+        # Only necessary in Py2
+        self.assertEqual(type(row._row_key), type(row_key_val))
+        self.assertEqual(row._row_key, row_key_val)
         self.assertTrue(row._table is table)
+        self.assertEqual(row._pb_mutations, [])
+
+    def test_constructor(self):
+        row_key = b'row_key'
+        self._constructor_helper(row_key)
+
+    def test_constructor_with_unicode(self):
+        row_key = u'row_key'
+        row_key_expected = b'row_key'
+        self._constructor_helper(row_key, row_key_expected=row_key_expected)
+
+    def test_constructor_with_non_bytes(self):
+        row_key = object()
+        with self.assertRaises(TypeError):
+            self._constructor_helper(row_key)
 
     def test_table_getter(self):
         table = object()
-        row = self._makeOne(table)
+        row = self._makeOne(b'row_key', table)
         self.assertTrue(row.table is table)
+
+    def test_row_key_getter(self):
+        row_key = b'row_key'
+        row = self._makeOne(row_key, object())
+        self.assertEqual(row.row_key, row_key)
